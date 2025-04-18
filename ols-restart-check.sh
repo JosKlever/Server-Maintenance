db9986f8c17c8914be8ad2e681b1c446e0e8814b
Jos Klever <josklever@gmail.com>
2025-04-18 15:34:41 +0200

    Update ols-restart-check.sh

--- ols-restart-check.sh
+++ ols-restart-check.sh
@@ -2,9 +2,9 @@
 #========================
 # Description: this script checks if .htaccess files have been changed in order to restart the Open LiteSpeed webserver
 # Environment: Almalinux, DirectAdmin (might need adjustments for other setups)
-# Save the script in /root/scripts/open-restart-check.sh and make sure you enter your own email address in the mail command.
+# Save the script in /root/scripts/ols-restart-check.sh and make sure you enter your own email address in the mail command.
 # Usage: call this script in /etc/cron.d/openlitespeed_htaccess_scan with the following line:
-# */3 * * * root /root/scripts/open-restart-check.sh
+# */3 * * * root /root/scripts/ols-restart-check.sh
 #========================
 changed_files=$(find /home/*/domains/*/public_html/ -maxdepth 2 -type f -newer /usr/local/lsws/cgid -name '.htaccess' 2>/dev/null)
 if [ -n "$changed_files" ]; then
